83f9ae43c19d9a7b9d90325fc01b59803f5ee9c8
theinhan8 <159587720+theinhan8@users.noreply.github.com>
2025-04-09 14:41:28 -0600

    Added the top navigation and logo to match Aldi's website while implementing our solution.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*Display menu links*/
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
