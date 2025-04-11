ee63f8f3a067de5fe63773314d28b5432f42efb9
Michu <michuarts13@gmail.com>
2025-04-10 21:02:04 -0500

    add javascript for input

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const nameInput = document.getElementById("name");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const userName = nameInput.value.trim();
+
+    if (userName) {
+      alert(`Thanks for subscribing, ${userName}!`); 
+    } else {
+      alert("Thanks for subscribing!");
+    }
+
+    form.reset();
+  });
+});
